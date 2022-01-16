7033333d9cafa701f583f646237657e3f4cdd1ca
sd8917 <sudhanshuraj8917@gmail.com>
2022-01-16 22:45:44 +0530

    first commit

---
+++ Intro_Linked_List.cpp/intro_LL.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+const unsigned int MOD = 1000000007;
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	cout << "Hello world" << endl;
+
+
+#ifndef ONLINE_JUDGE
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+
+	return 0;
+
+}
+
+/*
+ 0 1 2 3 4 5 6     7 8   9 10
+ w e l c o m e     t o   g e e k s f o r g e e k s welcome to geeksforgeeks
+
+*/
