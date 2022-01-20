b531be220caa435ec7a2a2a45e24615b3c422b37
sd8917 <sudhanshuraj8917@gmail.com>
2022-01-21 00:42:05 +0530

    detect Loop

---
+++ Detect_loop/Detect_loop.cpp
@@ -0,0 +1,189 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+const unsigned int MOD = 1000000007;
+using namespace std;
+
+//create a linked list..
+class node {
+public:
+	int data;
+	node*next;
+	node(int d)
+	{
+		data = d;
+		next = NULL;
+	}
+};
+
+int lengthLL(node*head)
+{
+	int len = 0;
+
+	while (head)
+	{
+		head = head->next;
+		len++;
+	}
+	return len;
+}
+
+void insertAtFront(node*&head, int data)
+{
+
+	if (head == NULL)
+	{
+		head = new node(data);
+		return;
+	}
+	else {
+		node*n = new node(data);
+		n->next = head;
+		head = n;
+	}
+
+}
+
+void insertAtEnd(node*&head, int data)
+{
+
+	if (head == NULL)
+	{
+		head = new node(data);
+		return;
+	}
+	else {
+		// to reach to the end of the linkedList for insertion..
+		node*temp = head;
+
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = new node(data);
+		temp->next->next = NULL;
+		return;
+	}
+
+}
+
+void InsertionAtPos(node* &head, int pos, int data) {
+	//if it at 0 means at front pos
+	if (pos == 0) {
+		insertAtFront(head, data);
+	}
+	else if (pos >= lengthLL(head) - 1) {
+		insertAtEnd(head, data);
+	}
+	else {
+		node* temp = head;
+		int count = 1;
+		while (count <= pos - 1) {
+			temp = temp->next;
+			count++;
+		}
+
+		node* n = new node(data);
+		n->next = temp->next;
+		temp->next = n;
+	}
+}
+
+
+//DETECTING OF THE LOOPS.
+bool detectLoop(node*&head)
+{
+	if (head == NULL) return false;
+	node*slow = head;
+	node*fast = head;
+
+	while (fast && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast) return true;
+	}
+
+	return false;
+}
+
+
+
+void printLL(node*head)
+{
+	while (head)
+	{
+		cout << head->data << "-> ";
+		head = head->next;
+	}
+	cout << "NULL" << endl;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	node*head = NULL;
+
+
+
+	insertAtFront(head, 2);
+	insertAtFront(head, 5);
+	insertAtFront(head, 7);
+	insertAtFront(head, 9);
+
+	printLL(head);
+
+	insertAtEnd(head, 12);
+	insertAtEnd(head, 15);
+	insertAtEnd(head, 17);
+	insertAtEnd(head, 30);
+
+	printLL(head);
+
+
+	InsertionAtPos(head, 4, 90);
+	printLL(head);
+
+
+	node*first = head;
+	node*temp = head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = head;
+
+	if (detectLoop(head))
+	{
+		cout << "There is a loop in the linked list" << endl;
+	}
+	else {
+		cout << "There is no loop in the linked list " << endl;
+	}
+
+	cout << "Removing of the loop from the linkedList " << endl;
+
+
+	printLL(head);
+
+#ifndef ONLINE_JUDGE
+	cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+
+	return 0;
+
+}
+
+/*
+ 0 1 2 3 4 5 6     7 8   9 10
+ w e l c o m e     t o   g e e k s f o r g e e k s welcome to geeksforgeeks
+
+*/
---
+++ Detect_loop/README.md
@@ -0,0 +1,78 @@
+#  ✨ Detect Loop in the Linked List ✨
+
+Check whether the linked list has a loop or not.
+
+
+```
+Circular linkedList means last node is connected to the first node of the linkedlist.
+```
+### Follow these steps : 
+ - If head == NULL , return false  Single node never form a circular Linked list .
+ - We take example of Cicular race track, and two Person A and B.  is running at speed of x and <br />
+ B is running with 2x speed.If the track is cicular then they must meet each other as some point, if the <br />
+ track is straight then they never.Considering this point we are writing the code.<br />
+ - Take two pointer ```slow ``` && ```fast```  both point to head.
+ - ```slow``` is increase by 1 step && ```fast``` is increased by 2 steps.
+ - if ``` slow == fast ``` then return true means we meet at a point .
+ - otherwise return false.
+
+### Code is :
+
+```
+
+bool detectLoop(node*&head)
+{
+	if (head == NULL) return false;
+	node*slow = head;
+	node*fast = head;
+
+	while (fast && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast) return true;
+	}
+
+	return false;
+}
+
+```
+
+
+
+# ✨ Remove Loop in the Linked List ✨
+
+
+First part we detected the loop and it has loop then for removing the we need to access the first and last node .
+
+### Follow these steps : 
+ - 
+
+### Code is :
+
+```
+
+bool detectLoop(node*&head)
+{
+	if (head == NULL) return false;
+	node*slow = head;
+	node*fast = head;
+
+	while (fast && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast) return true;
+	}
+
+	return false;
+}
+
+```
+
+
+
+> Note:```head ==NULL ``` special case for loop checking ... 
+
